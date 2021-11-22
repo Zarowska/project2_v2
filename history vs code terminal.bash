275afc3150a9a276805875dd8f9d8ccd96fee5fd
Oksna Żarowska <oksana.zarowska@gmail.com>
2021-11-22 02:06:47 +0100

    start v2

---
+++ history vs code terminal.bash
@@ -0,0 +1,52 @@
+npm install --save cors
+npm run build
+cd api         
+cd .\client\
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+cd .\myExpressApp\      
+cd vue-cli-service serve
+cd C:\Users\Oksana\Desktop\ZAI\project2\zarowoks\src\App.vue
+cd zarowoks\src      
+npm view ionic-native
+npm install https://github.com/ionic-team/ionic-native/tarball/v5.0.1 
+npm install bootstrap@next  
+npm install @vee-validate/rules
+vue-cli-service serve
+.\node_modules\.bin\vue-cli-service serve
+mkdir project2
+cd project2                         
+node app.js          
+npm --help              
+npm install -g express-generator    
+express myExpressApp --view pug     
+cd myExpressApp      
+npm install            
+npm start     
+code .               
+cd zarowoks     
+node --version         
+npm install -g yarn       
+yarn --version     
+yarn global add @vue/cli     
+vue create project2                 
+npm install -g @vue/cli           
+vue create zai2          
+vue cli    
+cd .\project2\
+cs zai2  
+npm install bootstrap  
+npm audit     
+npm install -S yup   
+npm install vee-validate --save     
+npm install vee-validate    
+yarn add vee-validate@next          
+npm run serve  
+cd .\zai2\   
+vue ui   
+mnp run server            
+run server    
+npm install --save @popperjs/core  
+cd zai2  
+cd zai               
+yarn serve                
+
